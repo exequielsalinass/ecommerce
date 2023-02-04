b2120634ce722a3d7552caa4184f76a31afa1ea8
Exequiel <e.x.e_salinas3@outlook.com>
2023-02-03 21:36:19 -0300

    Estilo css al topBar

--- components/Header/TopBar/TopBar.js
+++ components/Header/TopBar/TopBar.js
@@ -1,7 +1,39 @@
+import { Container, Grid, Image, Input } from "semantic-ui-react";
+import Link from "next/link";
+
 function TopBar() {
   return (
-    <div className="top-bar">TopBar..</div>
-  )
+    <div className="top-bar">
+      <Container>
+        <Grid className="top-bar">
+          <Grid.Column width={8} className="top-bar__left">
+            <Logo />
+          </Grid.Column>
+          <Grid.Column width={8} className="top-bar__right">
+            <Search/>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
-export default TopBar
+export default TopBar;
+
+function Logo() {
+  return (
+    <Link href="/">
+        <Image src="/logo.png" alt="Gaming" />
+    </Link>
+  );
+}
+
+function Search() {
+  return (
+    <Input
+      id="search-game"
+      icon={{name: "search"}}
+      placeholder="BUSCAR"
+    />
+  )
+}
